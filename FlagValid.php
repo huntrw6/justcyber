a03719d1dc42a1a1cbeff838f1d236817bc01c45
jsierracyber <122322516+jsierracyber@users.noreply.github.com>
2023-03-13 13:42:19 -0600

    Create FlagValid.php

---
+++ FlagValid.php
@@ -0,0 +1,14 @@
+<?php
+// Check if flag is valid
+$flag = $_POST['flag'];
+
+if ($flag === 'NOAH DID EVERYTHING JUST AS GOD COMMANDED HIM. GENESIS') {
+  $result = array('valid' => true);
+} else {
+  $result = array('valid' => false);
+}
+
+// Return JSON response
+header('Content-Type: application/json');
+echo json_encode($result);
+?>
