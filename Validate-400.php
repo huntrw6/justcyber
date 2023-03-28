bd87f9a1fe93b3f3c9d442c8e204039b2ea15d7e
muser4485 <122579317+muser4485@users.noreply.github.com>
2023-03-27 20:32:48 -0400

    initialcommit

--- Validate-400.php
+++ Validate-400.php
@@ -1,6 +1,6 @@
 <?php
 // Check if flag is valid
-$flag = ($_POST['flag']);
+$search = ($_POST['search']);
 
 if ($flag === 'NOAH DID EVERYTHING JUST AS GOD COMMANDED HIM. GENESIS') {
   $result = array('valid' => true);
@@ -8,7 +8,10 @@ if ($flag === 'NOAH DID EVERYTHING JUST AS GOD COMMANDED HIM. GENESIS') {
   $result = array('valid' => false);
 }
 
+// Return a response
+echo "<p>Search results for: " . $search . "</p>";
+?>
 // Return JSON response
-header('Content-Type: application/json');
-echo json_encode($result);
+//header('Content-Type: application/json');
+//echo json_encode($result);
 ?>
