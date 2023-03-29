55473d6ce2f380cd6db823eed7d0a6ec3192b1f5
muser4485 <122579317+muser4485@users.noreply.github.com>
2023-03-28 21:53:33 -0400

    initialcommit

--- 400-check.js
+++ 400-check.js
@@ -14,16 +14,16 @@
 
 
 
-//const grade = 90;
+const grade = 90;
 
-//if (grade >= 90) {
- // console.log("You got an A");
-//} else if (grade >= 80) {
-//  console.log("You got a B");
-//} else if (grade >= 70) {
-//  console.log("You got a C");
-//} else if (grade >= 60) {
-//  console.log("You got a D");
-//} else {
-//  console.log("You failed the class");
-//}
+if (grade >= 90) {
+ console.log("You got an A");
+} else if (grade >= 80) {
+  console.log("You got a B");
+} else if (grade >= 70) {
+  console.log("You got a C");
+} else if (grade >= 60) {
+  console.log("You got a D");
+} else {
+  console.log("You failed the class");
+}
