6e54794dd3bf6527d8e07ad35fd3958b61c0ff9b
Huntrw6 <44276639+huntrw6@users.noreply.github.com>
2023-03-30 15:14:18 -0700

    cs notation correction

--- checkmate.cs
+++ checkmate.cs
@@ -1,4 +1,6 @@
-[Title "White to move and win"]
+[Title "Stamma vs. Composition: White to move. Checkmate in 3."]
 [FEN "k6r/p2R4/P7/1R6/1N2pP2/1p2Pb2/4r3/5K2 w - - 0 1"]
 
-1. Rxg7+ Kxg7 2. Rg1+ Kf6 (2... Kh8 3. Qf6#) 3. Qh6+ Ke7 4. Qg5+ f6 5. Qc5+ Kf7 6. Nd6+ Ke7 7. Rg7+ Kd8 8. Nb7+ Ke8 9. Qe7# 
+// 1. Rxg7+ Kxg7 2. Rg1+ Kf6 (2... Kh8 3. Qf6#) 3. Qh6+ Ke7 4. Qg5+ f6 5. Qc5+ Kf7 6. Nd6+ Ke7 7. Rg7+ Kd8 8. Nb7+ Ke8 9. Qe7#
+1. Rh5+ Rh2 2. Rxh2+ Be2 3. Ke1+ Rg8 4. Nc6+ Rg1 5. Kxe2+ Rg2 6. Rxg2+ b2 7. Rxa7#
+1. Rh5+ Bxh5 2. Nc6+ Bf3 3. Rxa7#
