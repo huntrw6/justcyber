fa3c1e20a7ff5091c209922b59b3fdaa061f665f
Huntrw6 <44276639+huntrw6@users.noreply.github.com>
2023-03-30 14:29:24 -0700

    checkmate

---
+++ checkmate.cs
@@ -0,0 +1,4 @@
+[Title "White to move and win"]
+[FEN "k6r/p2R4/P7/1R6/1N2pP2/1p2Pb2/4r3/5K2 w - - 0 1"]
+
+1. Rxg7+ Kxg7 2. Rg1+ Kf6 (2... Kh8 3. Qf6#) 3. Qh6+ Ke7 4. Qg5+ f6 5. Qc5+ Kf7 6. Nd6+ Ke7 7. Rg7+ Kd8 8. Nb7+ Ke8 9. Qe7# 
