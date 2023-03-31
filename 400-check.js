6e7edf65731d3707dd9bd024e8cd54b800d1142a
muser4485 <122579317+muser4485@users.noreply.github.com>
2023-03-30 22:04:24 -0400

    initialcommit

--- 400-check.js
+++ 400-check.js
@@ -1,17 +1,11 @@
-function sendMessage() {
-
-    let filePath = "/Bible/Old Testament/Genesis/Chapter 1.txt";
-
-    
-
-    if (filePath.startsWith("/Bible/Old Testament/Genesis")) {
-      console.log("The file is in the Genesis directory.");
-    } else if (filePath.startsWith("/Bible/Old Testament/Exodus")) {
-      console.log("The file is in the Exodus directory.");
-    } else if (filePath.startsWith("/Bible/Old Testament/Leviticus")) {
-      console.log("The file is in the Leviticus directory.");
-    } else if (filePath.startsWith("/Bible/Old Testament/Numbers")) {
-      console.log("The file is in the Numbers directory.");
+function sendMessage(book, chapter, verse) {
+    if (book === "Genesis" && chapter === 1 && verse === 1) {
+      console.log("In the beginning God created the heavens and the earth.");
+    } else if (book === "Exodus" && chapter === 3 && verse === 14) {
+      console.log("And God said unto Moses, I AM THAT I AM: and he said, Thus shalt thou say unto the children of Israel, I AM hath sent me unto you.");
+    } else if (book === "Leviticus" && chapter === 19 && verse === 18) {
+      console.log("Thou shalt not avenge, nor bear any grudge against the children of thy people, but thou shalt love thy neighbour as thyself: I am the LORD.");
     } else {
-      console.log("The file is not in any of the directories.");
-    }};
+      console.log("Verse not found.");
+    }
+  }
