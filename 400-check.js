332765fe75e5298dd456e8801baa5744b822c175
muser4485 <122579317+muser4485@users.noreply.github.com>
2023-03-30 22:12:50 -0400

    initialcommit

--- 400-check.js
+++ 400-check.js
@@ -1,9 +1,9 @@
-function sendMessage(book, chapter, verse) {
-    if (book === "Genesis" && chapter === 1 && verse === 1) {
+function sendMessage(book) {
+    if (book === "Genesis") {
       console.log("In the beginning God created the heavens and the earth.");
-    } else if (book === "Exodus" && chapter === 3 && verse === 14) {
+    } else if (book === "Exodus") {
       console.log("And God said unto Moses, I AM THAT I AM: and he said, Thus shalt thou say unto the children of Israel, I AM hath sent me unto you.");
-    } else if (book === "Leviticus" && chapter === 19 && verse === 18) {
+    } else if (book === "Leviticus") {
       console.log("Thou shalt not avenge, nor bear any grudge against the children of thy people, but thou shalt love thy neighbour as thyself: I am the LORD.");
     } else {
       console.log("Verse not found.");
