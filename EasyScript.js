74a658e746adf60b450735e41dce4076b08978c0
jsierracyber <122322516+jsierracyber@users.noreply.github.com>
2023-04-10 15:39:32 -0600

    Create EasyScript.js

---
+++ EasyScript.js
@@ -0,0 +1,6 @@
+
+function sendMessage() {var encoded = "var input = document.getElementById("userInput").value;
+    
+    if (input === "NOAH DID EVERYTHING JUST AS GOD COMMANDED HIM. GENESIS") {
+        alert("Easy Flag");
+    }";var decodedString = atob(encoded);eval(decodedString);};
