0fc0958caab77ecb68264f514ee2f219bd4b7cfb
jsierracyber <122322516+jsierracyber@users.noreply.github.com>
2023-04-12 15:13:05 -0600

    Update EasyScript.js

--- EasyScript.js
+++ EasyScript.js
@@ -1,6 +1,1 @@
-
-function sendMessage() {var encoded = "var input = document.getElementById("userInput").value;
-    
-    if (input === "NOAH DID EVERYTHING JUST AS GOD COMMANDED HIM. GENESIS") {
-        alert("Easy Flag");
-    }";var decodedString = atob(encoded);eval(decodedString);};
+function sendMessage(){var encoded = "var input = document.getElementById("userInput").value; if (input === "NOAH DID EVERYTHING JUST AS GOD COMMANDED HIM. GENESIS") {alert("Easy Flag");}";var decodedString = atob(encoded);eval(decodedString);};
